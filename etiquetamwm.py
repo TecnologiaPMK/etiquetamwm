cd11e2e84358ff49884fffc5641a9db172d0d961
TecnologiaPMK <tecnologia@presmak.com.br>
2025-04-03 14:17:30 -0300

    Update etiquetamwm.py

--- etiquetamwm.py
+++ etiquetamwm.py
@@ -34,9 +34,9 @@ def create_label_image(data_fabricacao, part_number, nivel_liberacao, serial_fab
 
     # Carrega fontes
     font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
-    font_title = ImageFont.truetype(font_path, 30)
-    font_data = ImageFont.truetype(font_path, 30)
-    font_code = ImageFont.truetype(font_path, 42)
+    font_title = ImageFont.truetype(font_path, 5)
+    font_data = ImageFont.truetype(font_path, 5)
+    font_code = ImageFont.truetype(font_path, 5)
                            
     # Adiciona o logo
     logo = Image.open(logo_path)

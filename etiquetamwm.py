0f64d17fadb9f5c4f0ef3bce8a5889624fd9b508
TecnologiaPMK <tecnologia@presmak.com.br>
2025-04-03 14:13:15 -0300

    Update etiquetamwm.py

--- etiquetamwm.py
+++ etiquetamwm.py
@@ -33,9 +33,10 @@ def create_label_image(data_fabricacao, part_number, nivel_liberacao, serial_fab
     draw = ImageDraw.Draw(img)
 
     # Carrega fontes
-    font_title = ImageFont.load_default()
-    font_data = ImageFont.load_default()
-    font_code = ImageFont.load_default()
+    font_path = os.path.join(os.getcwd(), "arial.ttf")  # Caminho absoluto
+    font_title = ImageFont.truetype(font_path, 100)
+    font_data = ImageFont.truetype(font_path, 88)
+    font_code = ImageFont.truetype(font_path, 92)
 
     # Adiciona o logo
     logo = Image.open(logo_path)

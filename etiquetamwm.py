cc869427e56f82dcf2d5d3409af40dad0ffa7412
TecnologiaPMK <tecnologia@presmak.com.br>
2025-04-03 09:08:44 -0300

    Update etiquetamwm.py

--- etiquetamwm.py
+++ etiquetamwm.py
@@ -1,12 +1,11 @@
-import streamlit as st
-import datetime
+import segno
 import tempfile
 import os
-from PIL import Image, ImageDraw, ImageFont
-import treepoem
+import streamlit as st
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import mm
-import sys
+from PIL import Image, ImageDraw, ImageFont
+import datetime
 
 def is_streamlit_cloud():
     return os.environ.get("STREAMLIT_SERVER_RUNNING") is not None
@@ -31,57 +30,44 @@ def select_printer():
         return None
     return st.selectbox("Selecione a Impressora:", printers)
 
-def load_font(font_name, size):
-    try:
-        return ImageFont.truetype(font_name, size)
-    except IOError:
-        return ImageFont.load_default()
-
-def create_label_image(data_fabricacao, part_number, nivel_liberacao, serial_fabricacao, nf, logo_path, dpi=300, logo_position=(10, 10), text_offset=-30):
+def create_label_image(data_fabricacao, part_number, nivel_liberacao, serial_fabricacao, nf, logo_path, dpi=300):
     label_width, label_height = 110, 85  # mm
     width_pixels, height_pixels = (int(label_width * dpi / 25.4), int(label_height * dpi / 25.4))
     
     img = Image.new('RGB', (width_pixels, height_pixels), color='white')
     draw = ImageDraw.Draw(img)
     
-    font_title = load_font("arialbd.ttf", 45)
-    font_data = load_font("calibri.ttf", 40)
-    font_code = load_font("arialbd.ttf", 50)
+    font = ImageFont.load_default()
     
-    logo = Image.open(logo_path)
-    logo = logo.resize((500, 120))
-    img.paste(logo, logo_position)
-    y_pos = logo_position[1] + logo.size[1] + text_offset
+    try:
+        logo = Image.open(logo_path)
+        logo = logo.resize((500, 120))
+        img.paste(logo, (10, 10))
+    except:
+        pass
     
+    y_pos = 150
     info_texts = [
-        ("Data de Fabricação:", data_fabricacao.strftime('%d/%m/%Y')),
-        ("Part Number MWM:", part_number),
-        ("Nível de Liberação:", nivel_liberacao),
-        ("Serial de Fabricação:", serial_fabricacao),
-        ("Identificação do Fornecedor:", "13785"),
-        ("Número da NF:", nf),
+        f"Data de Fabricação: {data_fabricacao.strftime('%d/%m/%Y')}",
+        f"Part Number MWM: {part_number}",
+        f"Nível de Liberação: {nivel_liberacao}",
+        f"Serial de Fabricação: {serial_fabricacao}",
+        f"Número da NF: {nf}",
     ]
     
-    for title, value in info_texts:
-        draw.text((650, y_pos), title, fill="black", font=font_title)
-        y_pos += 45
-        draw.text((650, y_pos), value, fill="black", font=font_data)
-        y_pos += 65
-
-    # Gera o DataMatrix utilizando treepoem
-    dm_data = f"{data_fabricacao.strftime('%d/%m/%Y')};{part_number};{nivel_liberacao};{serial_fabricacao};13785;{nf}"
-    dm_img = treepoem.generate_barcode(barcode_type="datamatrix", data=dm_data)
-    dm_img = dm_img.convert("RGB")
-    dm_img = dm_img.resize((600, 400))
+    for text in info_texts:
+        draw.text((20, y_pos), text, fill="black", font=font)
+        y_pos += 30
     
-    dm_x, dm_y = 5, 200
-    img.paste(dm_img, (dm_x, dm_y))
+    dm_data = f"{data_fabricacao.strftime('%d/%m/%Y')};{part_number};{nivel_liberacao};{serial_fabricacao};{nf}"
+    qrcode = segno.make(dm_data)
+    qr_path = tempfile.NamedTemporaryFile(suffix=".png", delete=False).name
+    qrcode.save(qr_path, scale=10)
     
-    pr020_x = dm_x + dm_img.width // 2
-    pr020_y = dm_y + dm_img.height + 20
-    draw.text((pr020_x, pr020_y), PR_datamatrix, fill="black", font=font_code, anchor="mm")
+    dm_img = Image.open(qr_path)
+    dm_img = dm_img.resize((200, 200))
+    img.paste(dm_img, (20, y_pos))
     
-    img = img.rotate(90, expand=True)
     return img
 
 def save_as_pdf(img, quantity):
@@ -109,10 +95,10 @@ def print_pdf(pdf_path, printer_name):
         st.error(f"Erro ao imprimir: {str(e)}")
 
 dados_mwm = {
-    "7000448C93": {"nivel": "A", "serial": "13785", "datamatrix":"PR019"},
-    "7000666C93": {"nivel": "A", "serial": "13785", "datamatrix":"PR018"},
-    "961201150166": {"nivel": "A", "serial": "13785", "datamatrix":"PR020"},
-    "7000449C3": {"nivel": "A", "serial": "13785", "datamatrix":"PR023"},
+    "7000448C93": {"nivel": "A", "serial": "13785"},
+    "7000666C93": {"nivel": "A", "serial": "13785"},
+    "961201150166": {"nivel": "A", "serial": "13785"},
+    "7000449C3": {"nivel": "A", "serial": "13785"},
 }
 
 st.title("Etiquetas MWM")
@@ -122,12 +108,9 @@ part_number = st.selectbox("Part Number MWM:", list(dados_mwm.keys()))
 
 nivel_liberacao = st.text_input("Nível de Liberação:", value=dados_mwm[part_number]["nivel"])
 serial_fabricacao = st.text_input("Serial de Fabricação:", value=dados_mwm[part_number]["serial"])
-PR_datamatrix = st.text_input("Cod DataMatrix", value=dados_mwm[part_number]["datamatrix"])
-
 nf = st.text_input("Número da Nota Fiscal (NF):")
 quantidade = st.number_input("Quantidade de Etiquetas:", min_value=1, value=1, step=1)
 
-# Certifique-se de que o arquivo "logoPMK.png" esteja no repositório
 logo_path = "logoPMK.png"
 
 if st.button("Visualizar Prévia"):

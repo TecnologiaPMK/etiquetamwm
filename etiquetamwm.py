dd54820332323260ef46bf8ce832cea6579f27cc
TecnologiaPMK <tecnologia@presmak.com.br>
2025-04-03 15:50:12 -0300

    Update etiquetamwm.py

--- etiquetamwm.py
+++ etiquetamwm.py
@@ -42,7 +42,7 @@ def create_label_image(data_fabricacao, part_number, nivel_liberacao, serial_fab
     logo = Image.open(logo_path)
     logo = logo.resize((1200, 400))
     img.paste(logo, (70, 70))
-    y_pos = 550 # sobe as letras das informaçoes nao altera o logo
+    y_pos = 350 # sobe as letras das informaçoes nao altera o logo
 
     # Informações na etiqueta
     info_texts = [

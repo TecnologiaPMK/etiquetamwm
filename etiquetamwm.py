427c13bb9ae6027ac4e079d19ca09bdf3eb24593
TecnologiaPMK <tecnologia@presmak.com.br>
2025-04-03 11:25:41 -0300

    Update etiquetamwm.py

--- etiquetamwm.py
+++ etiquetamwm.py
@@ -27,7 +27,7 @@ def generate_datamatrix(data):
 def create_label_image(data_fabricacao, part_number, nivel_liberacao, serial_fabricacao, nf, logo_path, 
                        dpi=100, PR_datamatrix=""):
     label_width, label_height = 110, 85  # Dimensão da etiqueta em mm
-    width_pixels, height_pixels = (int(label_width * dpi / 25.4), int(label_height * dpi / 25.4))
+    width_pixels, height_pixels = (int(label_width * dpi / 25.8), int(label_height * dpi / 25.4))
     img = Image.new('RGB', (width_pixels, height_pixels), color='white')
     draw = ImageDraw.Draw(img)
 

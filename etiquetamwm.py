c5eb6f0d0e99d4b049c06215cb02cec4525ac247
TecnologiaPMK <tecnologia@presmak.com.br>
2025-05-05 07:38:54 -0300

    Update etiquetamwm.py

--- etiquetamwm.py
+++ etiquetamwm.py
@@ -64,7 +64,7 @@ def create_label_image(data_fabricacao, part_number, nivel_liberacao, serial_fab
     dm_data = f"{data_fabricacao.strftime('%d%m%Y')};{part_number};{nivel_liberacao};{serial_fabricacao};13785;{nf}"
     dm_img = generate_datamatrix(dm_data)
     dm_img = dm_img.resize((1300, 1300)) # altera o tamanho do datamatrix
-    img.paste(dm_img, (-10, 500))
+    img.paste(dm_img, (-2, 1000))
 
     # Código PR
     draw.text((650, 1800), PR_datamatrix, fill="black", font=font_code, anchor="mm")
